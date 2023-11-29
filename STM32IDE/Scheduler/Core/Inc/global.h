8a76968b8c0c559cc99d5816935f6627f40f1ca9
Fragalis <127601973+Fragalis@users.noreply.github.com>
2023-11-29 16:21:49 +0700

    very simple

---
+++ STM32IDE/Scheduler/Core/Inc/global.h
@@ -0,0 +1,22 @@
+/*
+ * global.h
+ *
+ *  Created on: Nov 29, 2023
+ *      Author: ACER
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include "main.h"
+
+#define SCH_MAX_TASKS 				40
+#define NO_TASK_ID					0
+
+#define ERROR_SCH_TOO_MANY_TASKS	10
+#define ERROR_SCH_TASK_NOT_FOUND	11
+
+#define RETURN_NORMAL				100
+#define RETURN_ERROR				101
+
+#endif /* INC_GLOBAL_H_ */
